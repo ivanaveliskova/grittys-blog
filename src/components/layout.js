fba680967bdf048201180346432c720933951db7
Ivana Veliskova <iveliskova@getguru.com>
2018-10-31 17:49:44 -0400

    Removes inlined styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,71 +1,32 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-import { rhythm, scale } from '../utils/typography'
+import React from "react";
+import { Link } from "gatsby";
 
 class Template extends React.Component {
   render() {
-    const { location, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+    const { location, children } = this.props;
+    const rootPath = `${__PATH_PREFIX__}/`;
+    let header;
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            Gatsby Starter Blog
-          </Link>
+        <h1>
+          <Link to={"/"}>Gatsby Starter Blog</Link>
         </h1>
-      )
+      );
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            Gatsby Starter Blog
-          </Link>
+        <h3>
+          <Link to={"/"}>Gatsby Starter Blog</Link>
         </h3>
-      )
+      );
     }
     return (
-      <div
-        style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <div>
         {header}
         {children}
       </div>
-    )
+    );
   }
 }
 
-export default Template
+export default Template;
